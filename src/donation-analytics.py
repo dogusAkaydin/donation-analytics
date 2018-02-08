9a7682857a45d687e3a176214f37a4d81629ee78
tjeckleburg <tjeck1925@gmail.com>
2018-02-08 12:56:31 -0800

    Major progress towards v1.

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -2,16 +2,16 @@
 
 import sys
 import csv
-from collections import Counter
+from collections import Counter, defaultdict
 
 def main(inFilePath,outFilePath):
     
     inFile  =open(inFilePath, 'r')
-    records =csv.reader(inFile,delimiter="|")
+    records =csv.reader(inFile,delimiter='|')
    
-    headerFilePath ="indiv_header_file.csv" #Downloaded from FEC website.    
+    headerFilePath ='indiv_header_file.csv' #Downloaded from FEC website.    
     with open(headerFilePath) as headerFile:
-        allColumns=next(csv.reader(headerFile,delimiter=","))
+        allColumns=next(csv.reader(headerFile,delimiter=','))
     
     columnNamesOfInterest=['CMTE_ID','NAME','ZIP_CODE',
                            'TRANSACTION_DT','TRANSACTION_AMT','OTHER_ID']
@@ -19,22 +19,56 @@ def main(inFilePath,outFilePath):
                                for aName in columnNamesOfInterest]
     
     colID = dict(zip(columnNamesOfInterest,columnNumbersOfInterest))
-   
-    donorCounter=Counter()
+  
+    donors = {} #For Method 1 and Method 3
+    #donors = defaultdict(set) #For Method 2
+    donorCounter=Counter() 
+    repeatDonations=defaultdict(list)
     for rec in records:
-        if valid(rec):
-            donorID=rec[colID['NAME']]+'|'+rec[colID['ZIP_CODE']][0:5]
-            donorCounter[donorID]+=1
+        if isValid(rec):
+            name     =rec[colID['NAME']]
+            zipCode  =rec[colID['ZIP_CODE']][0:5]
+            year     =rec[colID['TRANSACTION_DT']][4:]
+            receipent=rec[colID['CMTE_ID']]
+            amt      =rec[colID['TRANSACTION_AMT']]
+            #donorIDperYear=name+'|'+zipCode
+            donorID=name+'|'+zipCode
+            groupID=receipent+'|'+zipCode+'|'+year
+            #Method 1: setdefault
+            #donors.setdefault(donorID,[]).append(year)
+            #Method 2: defaultdict
+            #donors[donorID].add(year)
+            #Method 3: Build dict for donations while identifying repeat donors
+            #donors[donorID].append(year)
+            try: 
+               donors[donorID].append(year)
+               repeatDonations[groupID].append(int(amt))
+               updatePercentiles(repeatDonations)
+            except KeyError:
+               donors[donorID] = []
+               donors[donorID].append(year)
         else:
             continue
-        
-    print(donorCounter)
 
-
-def valid(record):
+def isValid(record):
     return True
 
-if __name__ == "__main__":
+def pctl(sortedList, percentile):
+    return percentile
+
+def updatePercentiles(donations):
+    for groupID, amounts in donations.items():
+        #This list is sorted except for the last appended item.
+        donations[groupID].sort()
+        sl=donations[groupID]
+        #I coud have used Insertion Sort but I'll let
+        #timsort do it -- because it can automatically 
+        #sense the sortedness of the list and choose the
+        #most efficient algorithm (Merge Sort or Insertion Sort)
+        print('{}|{}|{}|{}'.format(groupID,pctl(sl,pc),sum(sl),len(sl)))
+    return True
+
+if __name__ == '__main__':
     inFilePath  = sys.argv[1]
     outFilePath = sys.argv[2]
     main(inFilePath,outFilePath)
