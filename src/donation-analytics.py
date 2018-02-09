7499c7988b2c3eaa6b17fcebf5a9f4acd8422909
tjeckleburg <tjeck1925@gmail.com>
2018-02-08 20:59:43 -0800

    Nicefied v1. Added file output.

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -1,67 +1,72 @@
 """Compute running percentile of repeat donors in a list streaming line-by-line."""
 import sys
 import csv
-from collections import Counter, defaultdict
+from collections import defaultdict
 import math
 
-def main(inFilePath,pctFilePath,outFilePath):
- 
-    headerFilePath ='indiv_header_file.csv' #Downloaded from FEC website.    
-    with open(headerFilePath) as headerFile:
-        allColumns=next(csv.reader(headerFile,delimiter=','))
+def main(recFilePath,pctFilePath,outFilePath):
    
     with open(pctFilePath) as pctFile:
         percentile=int(pctFile.readline().rstrip())
+   
+    outFile = open(outFilePath, 'w')
+      
+    with open(recFilePath) as recFile:
+        records =csv.reader(recFile,delimiter='|')
+        receipentCol='CMTE_ID'
+        donorNameCol='NAME'
+        zipCodeCol  ='ZIP_CODE'
+        donDateCol  ='TRANSACTION_DT'
+        donAmtCol   ='TRANSACTION_AMT'
+        otherIDcol  ='OTHER_ID'
+        selectedColumns=[receipentCol,donorNameCol,zipCodeCol,donDateCol,donAmtCol,otherIDcol]  
+        colID = getColumnIDs(selectedColumns)
  
-    with  open(inFilePath) as inFile:
-        records =csv.reader(inFile,delimiter='|')
-         
-        columnNamesOfInterest=['CMTE_ID','NAME','ZIP_CODE',
-                               'TRANSACTION_DT','TRANSACTION_AMT','OTHER_ID']
-        
-        columnNumbersOfInterest = [allColumns.index(aName) 
-                                   for aName in columnNamesOfInterest]
-        
-        colID = dict(zip(columnNamesOfInterest,columnNumbersOfInterest))
-  
-        donors = {} #For Method 1 and Method 3
-        #donors = defaultdict(set) #For Method 2
-        donorCounter=Counter() 
-        repeatDonations=defaultdict(list)
+        donors={}
+        repeatDonations={}
+        #repeatDonations=defaultdict(list)
         for rec in records:
             if isValid(rec):
-                name     =rec[colID['NAME']]
-                zipCode  =rec[colID['ZIP_CODE']][0:5]
-                year     =rec[colID['TRANSACTION_DT']][4:]
-                receipent=rec[colID['CMTE_ID']]
-                amt      =rec[colID['TRANSACTION_AMT']]
-                #donorIDperYear=name+'|'+zipCode
-                donorID=name+'|'+zipCode
-                groupID=receipent+'|'+zipCode+'|'+year
-                #Method 1: setdefault
-                #donors.setdefault(donorID,[]).append(year)
-                #Method 2: defaultdict
-                #donors[donorID].add(year)
-                #Method 3: Build dict for donations while identifying repeat donors
-                #donors[donorID].append(year)
+                name     =rec[colID[donorNameCol]]
+                zipCode  =rec[colID[zipCodeCol]][0:5]
+                year     =rec[colID[donDateCol]][4:]
+                receipent=rec[colID[receipentCol]]
+                amt      =rec[colID[donAmtCol]]
+                donorID  =name+'|'+zipCode
+                groupID  =receipent+'|'+zipCode+'|'+year
+                if donorID in donors:
+                    repeatDonations.setdefault(groupID,[]).append(int(amt))
+                    #repeatDonations[groupID].append(int(amt))
+                    emitPercentileValues(repeatDonations,percentile,outFile)
+                else:
+                    donors[donorID] = set()
+                donors[donorID].add(year)
+                '''
                 try: 
                    donors[donorID].append(year)
                    repeatDonations[groupID].append(int(amt))
                    updatePercentiles(repeatDonations,percentile)
                 except KeyError:
-                   donors[donorID] = []
+                   donors[donorID]=[]
                    donors[donorID].append(year)
+                '''
             else:
                 continue
 
-def isValid(record):
-    return True
+def getColumnIDs(selectedColumns):
+    headerFilePath ='indiv_header_file.csv' #Downloaded from FEC website.    
+    with open(headerFilePath) as headerFile:
+        allColumns=next(csv.reader(headerFile,delimiter=','))
+      
+    selectedNumbers=[allColumns.index(aColumn) 
+                     for aColumn in selectedColumns]
+    return dict(zip(selectedColumns,selectedNumbers))
 
-def compPctl(sortedList,percentile,n):
+def findPercentileDonation(sortedList,percentile,n):
     idx=math.ceil((percentile/100.0)*n)-1 #-1 since the list is 0-based.
     return sortedList[idx]
 
-def updatePercentiles(donations,pc):
+def emitPercentileValues(donations,percentile,outFile):
     for groupID, amounts in donations.items():
         #This list is sorted except for the last appended item.
         donations[groupID].sort()
@@ -74,12 +79,16 @@ def updatePercentiles(donations,pc):
         sense the sortedness of the list and choose between 
         Merge Sort or Insertion Sort.
         '''
-        print('{}|{}|{}|{}'.format(groupID,compPctl(sl,pc,n),suml,n))
+        outFile.write('{}|{}|{}|{}\n'.format(groupID,
+                                      findPercentileDonation(sl,percentile,n),
+                                      suml,n))
+    return True
+
+def isValid(record):
     return True
 
 if __name__ == '__main__':
-    inFilePath  = sys.argv[1]
-    pctFilePath = sys.argv[2]
-    outFilePath = sys.argv[3]
-    main(inFilePath,pctFilePath,outFilePath)
-
+    recFilePath=sys.argv[1]
+    pctFilePath=sys.argv[2]
+    outFilePath=sys.argv[3]
+    main(recFilePath,pctFilePath,outFilePath)
