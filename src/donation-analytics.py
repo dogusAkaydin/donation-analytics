ab8da66dc9ad8c9afc2e21fc80ff2f64c9af0c0a
tjeckleburg <tjeck1925@gmail.com>
2018-02-11 14:29:33 -0800

    Removed an unnecessary loop in emitStats.

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -1,18 +1,26 @@
+#!/usr/bin/env python3
+'''
+Insight Data Engineering coding challenge:
+
+Compute running percentile of repeat donations to 
+US political campaigns based on FEC data.
+'''
 import sys
 import csv
 from collections import namedtuple
 import math
-import argparse
+import time
 import datetime
 
 def main(recFilePath,pctFilePath,outFilePath,logFilePath,logVerbose,modAccount):
+    t0wall = time.time()
     with open(pctFilePath, 'r') as pctFile, \
          open(recFilePath, 'r') as recFile, \
          open(outFilePath, 'w') as outFile, \
          open(logFilePath, 'w') as logFile:
          percentile = int(pctFile.readline().rstrip())
          divLine = '-'*79+'\n'
-         print('Processing. Logging results in {:s}'.format(logFilePath))
+         print('Processing. See process logs in {:s}'.format(logFilePath))
          logFile.write('Opened \n' 
                     '{0:s} \n' 
                     'to output the repeat donation stats in the '
@@ -35,6 +43,8 @@ def main(recFilePath,pctFilePath,outFilePath,logFilePath,logVerbose,modAccount):
          colID, nAllColumns = getColumnIDs(selectedColumns)
          donors = {}
          repeatDonations = {}
+         repeatCount = {}
+         repeatSum = {}
          #repeatDonations=defaultdict(list)
          nRec = 0
          nValid = 0
@@ -45,29 +55,58 @@ def main(recFilePath,pctFilePath,outFilePath,logFilePath,logVerbose,modAccount):
                 +divLine) \
                 .format(recFilePath)
          logFile.write(msg)
+         if not logVerbose:
+             msg = ('Use command line option -v to see verbose output here.\n')
+             logFile.write(msg)
+         #This loop is where the line-by-line inspection happens: 
          for rec in records:
+             t0_proc0 = time.time()
              nRec += 1
              ValidRecord = getRecord(rec,nRec,selectedColumns,colID,
-                                     nAllColumns,logFile)
+                                     nAllColumns,logFile,logVerbose)
              if ValidRecord:  
                  if ValidRecord.donorID in donors:
-                     repeatDonations.setdefault(ValidRecord.groupID,[]) \
-                                    .append(int(ValidRecord.amount))
+                     amt = int(ValidRecord.amount)
+                     repeatDonations.setdefault(ValidRecord.groupID,[]).append(amt)
+                     repeatCount[ValidRecord.groupID] = repeatCount.setdefault(ValidRecord.groupID,0) + 1
+                     repeatSum[ValidRecord.groupID]   = repeatSum.setdefault(ValidRecord.groupID,0) + amt
                      #repeatDonations[ValidRecord.groupID].append(int(amt))
-                     emitStats(repeatDonations,percentile,outFile)
+                     emit = True
+                     nSort    = len(repeatDonations[ValidRecord.groupID])
+                     t0_proc1 = time.time()
+                     emitStats(ValidRecord.groupID, 
+                               repeatDonations[ValidRecord.groupID],
+                               repeatCount[ValidRecord.groupID],
+                               repeatSum[ValidRecord.groupID],
+                               percentile,outFile,logFile)
+                     t1_proc1 = time.time()
                  else:
+                     emit = False
+                     nSort = 0
+                     t0_proc1 = time.time()
                      donors[ValidRecord.donorID] = set()
+                     t1_proc1 = time.time()
                  donors[ValidRecord.donorID].add(ValidRecord.year)
                  nValid += 1
              else:
                  nInvalid += 1
                  continue
-         
-         logFile.write(divLine+ 
-                      'Gone through a total of {0:d} records. \n' 
-                      'Processed {1:d} valid records. \n'
-                      'Skipped   {2:d} invalid records.\n'
-                      'DONE.'.format(nRec,nValid,nInvalid))
+             t1_proc0 = time.time()
+             dt_proc1 = t1_proc1 - t0_proc1
+             dt_proc0 = t1_proc0 - t0_proc0
+             ratio    = 100.0*dt_proc1/dt_proc0
+             msg=('{0:10d}: Emit: {1:2}, nSort = {5:7d}, dt_proc0 = {2:10f}, dt_proc1 = {3:10f}, ratio = {4:5.2f} \n' \
+                 .format(nRec,emit,dt_proc0,dt_proc1,ratio,nSort))
+             #logFile.write(msg)
+         dtWall = time.time() - t0wall
+         msg = (divLine+ 
+                'Gone through a total of {0:d} records. \n' 
+                'Processed {1:d} valid records. \n'
+                'Skipped   {2:d} invalid records.\n'
+                'DONE in {3:f} seconds of wall clock time '
+                .format(nRec,nValid,nInvalid,dtWall))
+         logFile.write(msg)
+    return dtWall
 
 def getColumnIDs(selectedColumns):
     headerFilePath = 'indiv_header_file.csv' #Downloaded from FEC website.    
@@ -90,27 +129,28 @@ def findPercentileValue(sortedList,percentile,n):
     idx = math.ceil((percentile/100.0)*n)-1 #-1 since the list is 0-based.
     return sortedList[idx]
 
-def emitStats(donations,percentile,outFile):
-    for groupID, amounts in donations.items():
+def emitStats(groupID, donations,cnt,tot,percentile,outFile,logFile):
+    #for groupID, amounts in donations.items():
         #This list is sorted except for the last appended item.
-        donations[groupID].sort()
-        sl = donations[groupID]
-        n = len(sl)
-        suml = sum(sl)
+        #donations[groupID].sort()
         #I coud have used an Insertion Sort routine here but I'll let timsort 
         #sort this out -- because it can automatically choose between
         #Merge Sort or Insertion Sort depending on sortedness of the input.
-        try:
-            outFile.write('{}|{}|{}|{}\n'.format(groupID,
-                           findPercentileValue(sl,percentile,n),suml,n))
-        except OSError as err:
-            path = os.path.realpath(outFile.name)
-            msg = ('Problem with output file {0}: {1} \n'.format(path,err))
-            logFile.write(msg)
-            raise OSError(msg)
+        #sortedList = donations[groupID]
+        #nL   = len(sortedList)
+        #sumL = sum(sortedList)
+        #outFile.write('{}|{}|{}|{}\n'.format(groupID,
+        #               findPercentileValue(sortedList,percentile,nL),sumL,nL))
+        #nL   = cnt[groupID]
+        #sumL = tot[groupID]
+        #outFile.write('{}|{}|{}|{}\n'.format(groupID,
+        #               findPercentileValue(donations[groupID],percentile,nL),sumL,nL))
+        donations.sort()
+        outFile.write('{}|{}|{}|{}\n'.format(groupID,
+                       findPercentileValue(donations,percentile,cnt),tot,cnt))
 
 def getRecord(record,lineNumber,selectedColumns,colID,
-              nAllColumns,logFile,logVerbose=False):
+              nAllColumns,logFile,logVerbose):
     Record = namedtuple('Record', 'lineNumber length otherID name ' 
                                   'fullZipCode date recipient amount')
     theRecord = Record(lineNumber = lineNumber,
@@ -132,44 +172,43 @@ def getRecord(record,lineNumber,selectedColumns,colID,
         return result(donorID,groupID,theRecord.amount,year)
 
 def isValid(nAllColumns,Record,logFile,logVerbose=False):
-
-    result = False
+    result = False #A record enters and remains invalid unless it passes all these tests:
     if Record.length != nAllColumns:
-        #Check how much impact logVerbose checks have on speed.
+        #Measure how much impact logVerbose checks have on speed.
         #Is there a way to bypass the checks entirely if logVerbose is False? 
         if logVerbose:
-            msg = ('Skipping line {:d}' 
+            msg = ('Skipping record line {:d} ' 
                    'because it has some missing fields.\n') \
-                  .format(lineNumber)
+                  .format(Record.lineNumber)
             logFile.write(msg)
     elif Record.otherID not in (None, ''):
         if logVerbose:
-            msg = ('Skipping line {:d}' 
+            msg = ('Skipping record {:d} ' 
                    'because "OTHER_ID" field is not empty.\n') \
-                  .format(lineNumber)
+                  .format(Record.lineNumber)
             logFile.write(msg)
     elif (Record.name is None or Record.name.isspace()):
         #Doing only a loose check on names on purpose.
         #If an aggressive check is needed a validator package can be used.
         if logVerbose:
-            msg = ('Skipping line {:d}' 
+            msg = ('Skipping record {:d} ' 
                    'because "NAME" field is blank. \n') \
-                  .format(lineNumber)
+                  .format(Record.lineNumber)
             logFile.write(msg)
     elif (len(Record.fullZipCode) < 5 
           or not Record.fullZipCode[0:5].isdecimal()): 
         #This will let in an entry like "94040-FOOBAR", which I thought is OK.
         if logVerbose:
-            msg = ('Skipping line {:d}' 
+            msg = ('Skipping record {:d} ' 
                    'because "ZIP_CODE" field is malformed. \n') \
-                  .format(lineNumber)
+                  .format(Record.lineNumber)
             logFile.write(msg)
-    elif (not isinstance(datetime.datetime.strptime(Record.date, '%m%d%Y'), 
-                         datetime.date)):
+    elif (Record.date is None or Record.date.isspace()):
         if logVerbose:
-            msg = ('Skipping line {:d}' 
-                   'because "TRANSACTION_DT" field is malformed. \n') \
-                  .format(lineNumber)
+            msg = ('Skipping record {:d} ' 
+                   'because "TRANSACTION_DT" field is empty \
+                                                   or it does not exist. \n') \
+                  .format(Record.lineNumber)
             logFile.write(msg)
     elif (Record.amount is None 
           or Record.amount.isspace()
@@ -178,19 +217,28 @@ def isValid(nAllColumns,Record,logFile,logVerbose=False):
         #Doing only a loose check on names on purpose.
         #If an aggressive check is needed a validator package can be used.
         if logVerbose:
-            msg = ('Skipping line {:d}' 
-                   'because "TRANSACTIO_AMT" field is malformed' 
-                                                     'or non-positive. \n') \
-                  .format(lineNumber)
+            msg = ('Skipping record {:d} ' 
+                   'because "TRANSACTION_AMT" field is malformed ' 
+                                                  'or non-positive. \n') \
+                  .format(Record.lineNumber)
             logFile.write(msg)
     elif Record.recipient is None or Record.recipient.isspace():
         if logVerbose:
-            msg = ('Skipping line {:d}'
+            msg = ('Skipping record {:d} '
                    'because CMTE_ID field is missing or empty \n') \
-                  .format(lineNumber)
+                  .format(Record.lineNumber)
             logFile.write(msg)
     else:
-        result=True
+        try: #One more check on date to catch it if malformed.
+            date=datetime.datetime.strptime(Record.date, '%m%d%Y')
+        except ValueError:
+            if logVerbose:
+                msg = ('Skipping record {:d} ' 
+                       'because "TRANSACTION_DT" field is malformed. \n') \
+                      .format(Record.lineNumber)
+                logFile.write(msg)
+        else:
+            result=True
 
     return result
 
@@ -202,25 +250,35 @@ def isNumber(s):
         return False
 
 if __name__ == '__main__':
-    '''
-    Compute runnig percentile of repeat donations to campaigns.
-    '''
+    '''Command-line execution for donation-analytics.py'''
+
+    import argparse
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('recFilePath', 
+                        help='Path to the record file provided by FEC')
+    parser.add_argument('pctFilePath', 
+                        help='Path to the file that holds the desired \
+                              percentile value between 0 and 100.')
+    parser.add_argument('outFilePath', 
+                        help='Path to the file where the repeat donation \
+                              results are written to.')
+    parser.add_argument('logFilePath', 
+                        help='Path to the file where a log of process \
+                             (stats, skipped lines, etc.) are written to.')
     parser.add_argument('-v','--v', action='store_true', 
-                        help='Output skipped record logs in the log file.')
+                        help='Outputs any skipped records in the log file.')
     parser.add_argument('-m','--m', action='store_true', 
                         help='Modifies the accounting so that the donations '
                              'done within the same calendar year are counted '
                              'as repeat donation.')
-    parser.add_argument('files', nargs='*')
     args = parser.parse_args()
     #Add some input checks here:
-    recFilePath = args.files[0]
-    pctFilePath = args.files[1]
-    outFilePath = args.files[2]
-    logFilePath = args.files[3]
     logVerbose = args.v 
     modAccount = args.m 
-    main(recFilePath,pctFilePath,outFilePath,logFilePath,
-         logVerbose,modAccount)
-    print('DONE.')
+    recFilePath = args.recFilePath
+    pctFilePath = args.pctFilePath
+    outFilePath = args.outFilePath
+    logFilePath = args.logFilePath
+    dtWall = main(recFilePath,pctFilePath,outFilePath,logFilePath,
+                  logVerbose,modAccount)
+    print('DONE in {0:10g} seconds of wall clock time'.format(dtWall))
